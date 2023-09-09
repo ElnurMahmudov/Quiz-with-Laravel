4bfe92502e9af1fa9c9d487b37f259caa74ab1cd
Elnur Mahmudov <mahmudoffgroup@gmail.com>
2023-09-09 20:30:23 +0400

    1.Created Quiz Migration 2.Created Factory Seed 3.The Seed structure was divided into tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,23 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::insert([
-            'name'=>'Elnur Mahmudov',
-            'email'=>'mahmudov_313@mail.ru',
-            'email_verified_at' => now(),
-            'type'=>'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'two_factor_secret' => null,
-            'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
-            'current_team_id' => null,
-        ]);
-
-         \App\Models\User::factory(5)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
+        ]);
     }
 }
