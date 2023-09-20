ee4749b73ddb4482d84bcc0f576bab5931dbe9f2
Elnur Mahmudov <mahmudoffgroup@gmail.com>
2023-09-20 17:56:26 +0400

    1.Quiz Result Calculation 2.Result Table

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use App\Models\Answer;
+use App\Models\Result;
+
 
 class MainController extends Controller
 {
@@ -23,6 +26,35 @@ class MainController extends Controller
     }
 
     public function result(Request $request, $slug){
-        return $request->post();
+        $quiz = Quiz::whereSlug($slug)->withCount('questions')->first() ?? abort();
+        $correct = 0;
+
+        foreach($quiz->questions as $question){
+            Answer::create([
+                'user_id' => auth()->user()->id,
+                'question_id' => $question->id,
+                'answer' => $request->post($question->id)
+            ]);
+
+            echo $question->correct_answer.'-'.$request->post($question->id).'<br>';
+
+           
+            if($question->correct_answer===$request->post($question->id)){
+                $correct+=1;
+            }
+        }
+
+        $point = round( (100 / count($quiz->questions) ) * $correct);
+        $wrong = count($quiz->questions) - $correct;
+
+        Result::create([
+            'user_id'=>auth()->user()->id,
+            'quiz_id'=>$quiz->id,
+            'point'=>$point,
+            'correct'=>$correct,
+            'wrong'=>$wrong,
+        ]);
+
+        return redirect()->route('quiz.detail',$quiz->slug)->withSuccess('The quiz was successfully completed. Point: '.$point);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             QuizSeeder::class,
             QuestionSeeder::class,
             AnswerSeeder::class,
+            ResultSeeder::class
         ]);
     }
 }
